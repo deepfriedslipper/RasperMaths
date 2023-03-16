d9c083c7abfdf1a293587fd3892dd5e6b9ca49bc
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2023-03-16 10:50:52 +0000

    Update login.js

--- login.js
+++ login.js
@@ -196,19 +196,7 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
       } });  }); }
 getData();
 
-function updateLike(message_id)
-{
-  console.log("clicked on like button - " + message_id);
-	button_id = message_id;
-	likes = document.getElementById(button_id).value;
-	updated_likes = Number(likes) + 1;
-	console.log(updated_likes);
 
-	firebase.database().ref(room_name).child(message_id).update({
-		like : updated_likes  
-	 });
-
-}
 
   
   function logout() {
