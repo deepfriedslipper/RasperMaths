fc02ab73b47293125813eddad21b00784f4e2447
deepfriedslipper <108072844+deepfriedslipper@users.noreply.github.com>
2023-05-25 21:13:30 +0100

    Update easy.js

--- lesson/easy.js
+++ lesson/easy.js
@@ -1,50 +1,17 @@
 const question = document.querySelector("#question_title");
-const fs = require('fs');
 
-// Function to read a CSV file
-function readCSVFile(filename) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      const lines = data.trim().split('\n');
-      resolve(lines);
+function choose_question(topic){
+  fetch(topic + '/easy_multiple_choice.csv')
+    .then(response => response.text())
+    .then(data => {
+      const lines = data.split('\n');
+      lines.shift();
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      const randomLine = lines[randomIndex];
+    
+      question.textContent = randomLine;
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-  });
-}
-
-
-function pickRandomLine(lines) {
-  const randomIndex = Math.floor(Math.random() * (lines.length - 1)) + 1;
-  return lines[randomIndex];
-}
-
-function begin_easy(subject) {
-  var question_type = Math.floor(Math.random() * 2);
-  if (question_type == 1) {
-    const csvFilePath = subject + '/easy_multiple_choice.csv';
-
-    readCSVFile(csvFilePath)
-      .then(lines => {
-        const randomLine = pickRandomLine(lines);
-        console.log('Random Line:', randomLine);
-      })
-      .catch(err => {
-        console.error('Error:', err);
-      });
-  } else {
-    const csvFilePath = subject + '/easy_questions.csv';
-
-    readCSVFile(csvFilePath)
-      .then(lines => {
-        const randomLine = pickRandomLine(lines);
-        console.log('Random Line:', randomLine);
-      })
-      .catch(err => {
-        console.error('Error:', err);
-      });
-  }
 }
