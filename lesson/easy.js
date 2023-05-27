27666bdc8dce7f8ca5ffa013e0afefe335452a0a
RasperMaths <131694670+RasperMaths@users.noreply.github.com>
2023-05-27 16:05:12 +0100

    Update easy.js

--- lesson/easy.js
+++ lesson/easy.js
@@ -1,21 +1,17 @@
 const question = document.querySelector(".question_title");
 
-function choose_question(var topic){
-  fetch(topic + '/easy_multiple_choice.csv')
+function choose_question(topic) {
+  fetch('enlargement/easy_multiple_choice.csv')
     .then(response => response.text())
     .then(data => {
       const lines = data.split('\n');
       lines.shift();
       const randomIndex = Math.floor(Math.random() * lines.length);
       const randomLine = lines[randomIndex];
-    
+
       question.textContent = randomLine;
     })
     .catch(error => {
       console.error('Error:', error);
     });
-}
-
-function begin_easy(var topic){
-  choose_question(topic);
 }
