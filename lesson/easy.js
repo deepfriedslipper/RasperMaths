c314ad7e2c1f1ee860548b331cb2852fa3f657fb
RasperMaths <131694670+RasperMaths@users.noreply.github.com>
2023-06-14 17:24:56 +0100

    u

--- lesson/easy.js
+++ lesson/easy.js
@@ -3,12 +3,16 @@ const option_1 = document.querySelector(".option1");
 const option_2 = document.querySelector(".option2");
 const option_3 = document.querySelector(".option3");
 const option_4 = document.querySelector(".option4");
+const option_1_wrapper = document.querySelector(".option1-wrapper");
+const option_2_wrapper = document.querySelector(".option2-wrapper");
+const option_3_wrapper = document.querySelector(".option3-wrapper");
+const option_4_wrapper = document.querySelector(".option4-wrapper");
 const img = document.querySelector(".question_img");
 const input_box = document.querySelector(".input-box");
 
 function choose_question() {
   var num = Math.floor(Math.random() * 2);
-  if (num == 1){
+  if (num == 1) {
     multiple_choice();
   } else {
     input();
@@ -17,41 +21,12 @@ function choose_question() {
 
 function input() {
   input_box.style.display = 'inline-block';
-  option_1.style.display = 'none';
-  option_2.style.display = 'none';
-  option_3.style.display = 'none';
-  option_4.style.display = 'none';
-  
+  option_1_wrapper.style.display = 'none';
+  option_2_wrapper.style.display = 'none';
+  option_3_wrapper.style.display = 'none';
+  option_4_wrapper.style.display = 'none';
+
   fetch('easy.csv')
-  .then(response => response.text())
-  .then(data => {
-    const lines = data.split('\n');
-    lines.shift();
-    const randomIndex = Math.floor(Math.random() * lines.length);
-    const randomLine = lines[randomIndex];
-    lines.splice(randomIndex, 1);
-    var array = randomLine.split(",");
-    question.textContent = array[0];
-
-    if (array[1] != 'null') {
-      img.src = array[1];
-    } else {
-      img.src = null;
-    }
-  })
-  .catch(error => {
-    question.textContent = 'Error: ' + error;
-  });
-}
-
-function multiple_choice() {
-    input_box.style.display = 'none';
-    option_1.style.display = 'block';
-    option_2.style.display = 'block';
-    option_3.style.display = 'block';
-    option_4.style.display = 'block';
-  
-    fetch('easy_multiple_choice.csv')
     .then(response => response.text())
     .then(data => {
       const lines = data.split('\n');
@@ -60,11 +35,46 @@ function multiple_choice() {
       const randomLine = lines[randomIndex];
       lines.splice(randomIndex, 1);
       var array = randomLine.split(",");
+      console.log(array[0]);
       question.textContent = array[0];
 
       if (array[1] != 'null') {
+        console.log("Is not null");
         img.src = array[1];
       } else {
+        console.log("Is null");
+        img.src = null;
+      }
+    })
+    .catch(error => {
+      question.textContent = 'Error: ' + error;
+    });
+}
+
+function multiple_choice() {
+  input_box.style.display = 'none';
+  option_1_wrapper.style.display = 'flex';
+  option_2_wrapper.style.display = 'flex';
+  option_3_wrapper.style.display = 'flex';
+  option_4_wrapper.style.display = 'flex';
+
+  fetch('easy_multiple_choice.csv')
+    .then(response => response.text())
+    .then(data => {
+      const lines = data.split('\n');
+      lines.shift();
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      const randomLine = lines[randomIndex];
+      lines.splice(randomIndex, 1);
+      var array = randomLine.split(",");
+      console.log(array[0]);
+      question.textContent = array[0];
+
+      if (array[1] != 'null') {
+        console.log("Is not null");
+        img.src = array[1];
+      } else {
+        console.log("Is null");
         img.src = null;
       }
 
